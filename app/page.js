23168fe0b8f03672f7b284e1fe59ca06dae0e4e0
masumtradingcobd-stack <masumtradingcobd@gmail.com>
2026-04-28 16:50:34 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Masum Trading Hub</h1>
+      <p>Your FBA & Wholesale Website is Live 🚀</p>
+    </div>
+  );
+}
